3cb08f20f2f085f4dec2438f62e2465b1c0898c7
aleixgarcia-r <125563271+aleixgarcia-r@users.noreply.github.com>
2023-04-25 09:57:31 +0200

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,3 +1,29 @@
 ######
 # ibm_resource_group
 ######
+variable "name" {
+  description = "name"
+  type = string
+  default = ""
+}
+variable "resource_group_id" {
+  description = "resource_group_id"
+  type = string
+  default = ""
+}
+variable "service" {
+  description = "service"
+  type = string
+  default = ""
+}
+variable "plan" {
+  description = "plan"
+  type = string
+  default = ""
+}
+variable "location" {
+  description = "location"
+  type = string
+  default = ""
+}
+
